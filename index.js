17cebe47426774b5205b204820be5f3379540759
Muhammad Umair Shaikh <64693746+umarshaikh64@users.noreply.github.com>
2023-08-09 03:23:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-const http = require('http');
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const path = require('path');
+
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+const app = express();
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// https://youtube.com/shorts/UHHEPlIEO6Y?feature=share
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+})
+
+app.post("/youtube", async (req, res) => {
+    const url = req.body.url;
+    try {
+        const info = await ytdl.getInfo(url);
+        res.status(200).json({
+            status: true,
+            code: 200,
+            data: info,
+            formats: info.formats
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            code: 500,
+            error: error
+        })
+    }
+    // ytdl(url)
+    // .pipe(fs.createWriteStream('video.mp4')).on("finish",()=>{
+    //     res.send("ok");
+    // }).on("error",(err)=>{
+    //     res.send(err);
+    // });
+})
+
+
+app.listen(PORT, () => {
+    console.log("server is running on Port 3000");
+})
